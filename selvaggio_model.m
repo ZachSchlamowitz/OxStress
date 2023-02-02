7cc66e51688d207cb638da870981a291763e2cd9
Zach Schlamowitz <zschlamowitz@email.arizona.edu>
2023-02-01 19:30:03 -0700

    Fixed parameter units (to agree)

--- selvaggio_model.m
+++ selvaggio_model.m
@@ -42,44 +42,53 @@
 % Initialize parameters
 global Params
 Params = struct();
-Params.v_sup = 10^-5; % (M/sec) ranges 10^-7 to 10^-3 in Fig 3, based on varying levels of extracellular H202
+Params.v_sup = 10^(-5+6); % (µM/sec) (converted from M/sec) ranges 10^-7 to 10^-3 in Fig 3, based on varying levels of extracellular H202
 Params.k_Alt = 79; % (sec^-1)
-Params.k_Ox = 4e7; % (M^-1 sec^-1)
-Params.k_Sulf = 5.1e3; % (M^-1 sec^-1)
-Params.k_Srx = 3.3; % (10^-3 sec^-1) FLAG
+Params.k_Ox = 40; % (µM^-1 sec^-1) (converted from 4e7 M^-1 sec^-1)
+Params.k_Sulf = 5.1e-3; % (µM^-1 sec^-1) (converted from M^-1 sec^-1)
+Params.k_Srx = 3.3e-3; % (sec^-1) (converted from 10^-3 sec^-1)
 Params.k_Cond = 6.4; % (sec^-1)
-Params.k_Red = 2.1e5; % (M^-1 sec^-1)
-Params.VAppMax = 0.23; % (mM/sec) NOTE that is reported as VMax in Table 2
+Params.k_Red = 0.21; % (µM^-1 sec^-1) (converted from M^-1 sec^-1)
+Params.VAppMax = 230; % (µM/sec) (converted from 0.23 mM/sec) NOTE that is reported as VMax in Table 2; presumably interchangeable?
 Params.K_M = 1.8; % (µM)
 Params.PrxTotal = 92; % total concentration?? FLAG of Prx (µM)
 Params.TrxTotal = 23; % total concentration?? FLAG of Trx (µM)
 
-
-
-
+% Run Model
 t0 = 0;
-tf = 10;
-initvals = [1; 1; 1; 1; 1];
+tf = 50; % seconds?
+% initvals = [1; 23; 23; 23; 11.5]; % For rough initial guess, start with 1µM H202 and divide the total Prx and Trx values equally across states; [PrxX] = 92/4 = 23. [TrxX] = 23/2 = 11.5
+initvals = [10; 0.1; 0.1; 0.1; 0.1];
 %opts = odeset('RelTol',1e-2, 'AbsTol',1e-5, 'InitialStep',0.1, 'MaxStep',0.1);
 tic
-[time, sol] = ode15s(@selvaggio, [t0 tf], initvals);%, opts);
+[time, sol] = ode23s(@selvaggio, [t0 tf], initvals);%, opts);
 toc
-disp(strcat('Time elapsed: ', num2str(toc)))
+% disp(strcat('Time elapsed: ', num2str(toc)))
+
+% Obtain Trx-SH and Prx-S time courses; add as columns to sol
+temp = sol'; 
+temp = [temp; zeros(2, size(sol,1))];
+% Note that we back out PrxS from total PrxTotal = PrxS + PrxSO + PrxSS + PrxSO2
+temp(6,:) = Params.PrxTotal - temp(2,:) - temp(3,:) - temp(4,:);
+% and similarly we back out TrxSH from total TrxTotal = TrxSH + TrxSS.
+temp(7,:) = Params.TrxTotal - temp(5,:);
+sol = temp';
 
 % Time Series Plot
 plot(time, sol, LineWidth=2)
 % title('Predator, Prey Populations Over Time')
 xlabel('t')
 % ylabel('Population')
-legend('H2O2', 'PrxSO', 'PrxSO2', 'PrxSS', 'TrxSS','Location', 'North', 'FontSize',14 )
+legend('H2O2', 'PrxSO', 'PrxSO2', 'PrxSS', 'TrxSS', 'PrxS','TrxSH', 'Location', 'North', 'FontSize',14 )
 
 figure
 
-plot(time(1:250), sol(1:250, :), LineWidth=2)
+beginning = ceil(.75*length(time));
+plot(time(1:beginning), sol(1:beginning, :), LineWidth=2)
 % title('Predator, Prey Populations Over Time')
 xlabel('t')
 % ylabel('Population')
-legend('H2O2', 'PrxSO', 'PrxSO2', 'PrxSS', 'TrxSS','Location', 'North', 'FontSize',14 )
+legend('H2O2', 'PrxSO', 'PrxSO2', 'PrxSS', 'TrxSS','PrxS','TrxSH','Location', 'North', 'FontSize',14 )
 
 function eqs = selvaggio(t,vars)
     global Params
